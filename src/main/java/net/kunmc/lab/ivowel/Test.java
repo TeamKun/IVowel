966303fa0580be5c970f76716bd6b5a469896744
MORIMORI0317 <morimori.0317@outlook.jp>
2022-06-04 04:18:29 +0900

    久しぶりすぎてこれもうわかんねぇな

--- src/main/java/net/kunmc/lab/ivowel/Test.java
+++ src/main/java/net/kunmc/lab/ivowel/Test.java
@@ -3,10 +3,8 @@ package net.kunmc.lab.ivowel;
 public class Test {
 
     public static void main(String[] args) {
-        //JapaneseManager.getInstance().init();
-        //   System.out.println(JapaneseManager.getInstance().convertVowelOnly("課長壊れる", true, JapaneseManager.LeaveType.NOT_CNV, JapaneseManager.LeaveType.MIN));
-        //    System.out.println(JapaneseManager.getInstance().convertVowelOnly("ｳｧｧ!!ｵﾚﾓｲｯﾁｬｳｩｩｩ!!!ｳｳｳｳｳｳｳｳｳｩｩｩｩｩｩｩｩｳｳｳｳｳｳｳｳ!ｲｨｨｲｨｨｨｲｲｲｨｲｲｲｲ", true, JapaneseManager.LeaveType.NOT_CNV, JapaneseManager.LeaveType.MIN));
-        //System.out.println(JapaneseManager.getInstance().convertHiragana("イメージ", false));
+        //String ret = VowelManager.getInstance().convertHiragana("課長壊れる～", false);
+        System.out.println(VowelManager.getInstance().convertVowelOnly("課長壊れる～", false));
     }
 
 
